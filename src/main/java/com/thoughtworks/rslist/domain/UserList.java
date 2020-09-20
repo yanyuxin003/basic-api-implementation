40813c13e003bb073e7a3c40b718fa3a8ec8b453
yanyuxin <617702281@qq.com>
2020-09-21 07:42:50 +0800

    merge

--- src/main/java/com/thoughtworks/rslist/domain/UserList.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.rslist.domain;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class UserList {
-    public static List<User> userList = new ArrayList<>();
-
-}
